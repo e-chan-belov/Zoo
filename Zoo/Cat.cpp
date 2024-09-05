fcf894099a3d946129b304c127dd3ff61165d6b2
e-chan-belov <echanbelov@icloud.com>
2024-09-05 12:07:37 +0300

    final

--- Zoo/Cat.cpp
+++ Zoo/Cat.cpp
@@ -1,3 +1,3 @@
-#include "Cat.h"
+#include "Cat.hpp"
 
 
--- Zoo/main.cpp
+++ Zoo/main.cpp
@@ -1,11 +1,15 @@
 
 #include <iostream>
-#include "Animal.h"
+#include "Cat.hpp"
+#include "Elephant.hpp"
+#include "Alien.hpp"
+
 
 int main(int argc, const char * argv[]) {
 
+    Alien a("white", 2, "space", 1000);
+    std::cout << a.voice();
     
     
-    std::cout << "Hello, World!\n";
     return 0;
 }
