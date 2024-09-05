b8c8e315a8a0169f2e7d3f1d7767a289206f5e75
e-chan-belov <echanbelov@icloud.com>
2024-09-05 10:57:21 +0300

    Initial Commit

---
+++ Zoo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Zoo
+//
+//  Created by Белов Егор on 05.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
